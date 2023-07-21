ee07265ea6f9e758a273ed4c50d2ee168d55c22a
Tilivaldi Amrayev <tilvaldi@mail.ru>
2023-07-22 02:50:58 +0600

    New install

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        mainMethodShowMessageAble();
+        mainMethodShowInfo();
+    }
+    private static void mainMethodShowMessageAble() {
+        System.out.println("Hello able QA course people\n");
+    }
+    private static void mainMethodShowInfo() {
+        System.out.println("new changes");
+    }
+}
