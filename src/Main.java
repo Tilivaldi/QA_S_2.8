752c1ffc64a36608f8b89a84a1460d3a2109d5c6
Tilivaldi Amrayev <tilvaldi@mail.ru>
2023-07-22 02:59:19 +0600

    New branches

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,16 @@ public class Main {
     public static void main(String[] args) {
         mainMethodShowMessageAble();
         mainMethodShowInfo();
+        mainMethodShowMessageAbleBue();
     }
     private static void mainMethodShowMessageAble() {
         System.out.println("Hello able QA course people\n");
     }
     private static void mainMethodShowInfo() {
-        System.out.println("new changes");
+        System.out.println("new changes - is new life?");
+    }
+
+    private static void mainMethodShowMessageAbleBue() {
+        System.out.println("Good buy able QA course people\n");
     }
 }
